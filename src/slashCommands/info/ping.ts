21f3d748917c6e47a5f500006916fcbc3d2a51da
dragongoose <dragon@dragongoose.xyz>
2022-05-07 14:03:51 -0400

    Add exmaple autocomplete

--- src/slashCommands/info/ping.ts
+++ src/slashCommands/info/ping.ts
@@ -1,11 +1,22 @@
 // eslint-disable-next-line prettier/prettier
-import { Client, CommandInteraction } from 'discord.js';
+import {Client, CommandInteraction} from 'discord.js';
 import {CommandType} from '../../types';
 
 export const ping = {
   name: 'ping',
   description: 'returns websocket ping',
   type: CommandType.ChatInput,
+  options: [
+    {
+      name: 'example',
+      description: 'This is a example option to demonstrate autocomplete',
+      type: 3,
+      autocomplete: true,
+    },
+  ],
+  autocomplete: {
+    example: ['Example 1', 'Example 2', 'Example 3'],
+  },
   /**
    *
    * @param {Client} client
