e0d5292af1025141d0aec4214e3d97cce7b63046
dragongoose <dragon@dragongoose.xyz>
2022-05-07 19:57:02 -0400

    Remove unused types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,5 @@
 export * from './SlashCommand';
 export * from './Data';
 export * from './Config';
-export * from './CommandProperties';
-export * from './CommandType';
 export * from './AutocompleteChoices';
 export * from './Command';
