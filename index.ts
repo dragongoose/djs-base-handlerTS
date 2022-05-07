347f3b230222ff9679474c5561c79c2850ea915c
dragongoose <dragon@dragongoose.xyz>
2022-05-06 21:30:13 -0400

    Converted index.js to TS

---
+++ index.ts
@@ -0,0 +1,29 @@
+import {Client, Collection} from 'discord.js';
+import {config} from './config';
+import {Command} from './src/types';
+
+export const client = new Client({
+  intents: config.intents,
+});
+
+export const data = {
+  config,
+  commands: new Collection() as Collection<string, Command>,
+  slashCommands: new Collection() as Collection<string, Command>,
+};
+
+// Initializing the project
+import handler from './src/handler';
+handler(client, data);
+
+client.login(config.token);
+
+// This recode of this repo took 3:10:42 to complete.
+// I converted everything from JS to TS.
+// I also added some comments.
+// That's besides the point though.
+// I was stupid, and took no brakes.
+// It took ~1.5k errors.
+// 158 revisions.
+// A lot of confusion.
+// and my sanity.
