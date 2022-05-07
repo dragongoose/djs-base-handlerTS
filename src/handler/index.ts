9024fe8fa90accb524dba56fc220f96c26c3fb84
dragongoose <dragon@dragongoose.xyz>
2022-05-07 19:58:46 -0400

    Generate help command

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -1,10 +1,11 @@
-import {Client} from 'discord.js';
+import {Client, Message, MessageEmbed} from 'discord.js';
 import mongoose from 'mongoose';
 import * as cmds from '../commands';
 import * as slashcmds from '../slashCommands';
-import {Command, SlashCommand, CommandProperties, Data} from '../types';
+import {Command, SlashCommand, Data} from '../types';
 import * as path from 'path';
 import * as fs from 'fs';
+import * as chalk from 'chalk';
 
 /**
  * @param {Client} client
@@ -14,14 +15,77 @@ export default async (client: Client, data: Data) => {
 
   for (let i = 0; i < commandArray.length; i++) {
     const command: Command = commandArray[i];
+    console.log(command);
 
     if (command.name) {
-      const properties: CommandProperties = {...command};
-      data.commands.set(command.name, properties);
+      data.commands.set(command.name, command);
+      console.log(chalk.cyan('[!] Loaded command '), chalk.green(command.name));
     }
   }
 
-  const arrayOfSlashCommands: SlashCommand[] = [];
+  // Generate help command
+  const helpCommand: Command = {
+    name: 'help',
+    aliases: ['h'],
+    description: 'Get help for a command',
+    usage: 'help <command>',
+
+    run: async (client: Client, message: Message, args: string[]) => {
+      if (args.length === 0) {
+        const embed = new MessageEmbed()
+          .setTitle('Help')
+          .setDescription(
+            `Use \`${data.config.prefix}help [command name]\` to get help for a command.`
+          )
+          .addFields(
+            {
+              name: 'Commands',
+              value: data.commands
+                .map(command => {
+                  return `\`${command.name}\``;
+                })
+                .join('\n'),
+              inline: true,
+            },
+            {
+              name: 'Aliases',
+              value: data.commands
+                .map(command => command.aliases!.join('\n'))
+                .join('\n'),
+              inline: true,
+            }
+          )
+          .setColor('#0099ff');
+        message.channel.send({embeds: [embed]});
+      } else {
+        const command = data.commands.get(args[0]);
+        if (command) {
+          const embed = new MessageEmbed()
+
+            .setTitle(`Help for ${command.name}`)
+            .setDescription(command.description)
+            .addFields(
+              {
+                name: 'Aliases',
+                value: command.aliases!.join('\n'),
+                inline: true,
+              },
+              {
+                name: 'Usage',
+                value: command.usage,
+                inline: true,
+              }
+            )
+            .setColor('#0099ff');
+          message.channel.send({embeds: [embed]});
+        } else {
+          message.channel.send(`Command ${args[0]} not found.`);
+        }
+      }
+    },
+  };
+  data.commands.set(helpCommand.name, helpCommand);
+  console.log(chalk.cyan('[!] Loaded command '), chalk.green(helpCommand.name));
 
   //Events
   const dir = path.join(__dirname, '../events');
@@ -34,6 +98,8 @@ export default async (client: Client, data: Data) => {
   }
 
   // Slash Commands
+  const arrayOfSlashCommands: SlashCommand[] = [];
+
   const slashArray = [...Object.values(slashcmds)];
   for (let i = 0; i < slashArray.length; i++) {
     const command: SlashCommand = slashArray[i];
@@ -41,6 +107,10 @@ export default async (client: Client, data: Data) => {
     if (command.name) {
       data.slashCommands.set(command.name, command);
       arrayOfSlashCommands.push(command);
+      console.log(
+        chalk.cyan('[!] Loaded slash command '),
+        chalk.green(command.name)
+      );
     }
   }
 
