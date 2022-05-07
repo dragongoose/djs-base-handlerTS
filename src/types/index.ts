706491c8198740487a3b5818825af29bc77e7bf1
dragongoose <dragon@dragongoose.xyz>
2022-05-06 21:35:43 -0400

    exports

---
+++ src/types/index.ts
@@ -0,0 +1,6 @@
+export * from './Command';
+export * from './Data';
+export * from './Config';
+export * from './CommandProperties';
+export * from './CommandType';
+export * from './AutocompleteChoices';
