d3787e307e7096cf7034f1cee68f0ea2c27af42c
dragongoose <dragon@dragongoose.xyz>
2022-05-07 20:50:11 -0400

    Chalkify the ready message

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,5 +1,10 @@
 import {client} from '../../index';
+import * as chalk from 'chalk';
 
 client.on('ready', () =>
-  console.log(`${client.user!.tag} is up and ready to go!`)
+  console.log(
+    `${chalk.cyan('[!]')} ${chalk.magenta(client.user!.tag)} ${chalk.cyan(
+      'is up and ready to go!'
+    )}`
+  )
 );
